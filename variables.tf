ff08b07efb5320ba7b18143469f3febde52dcba2
Mihai Alexandru Andrei <MihaiAlexandru.Andrei@endava.com>
2025-11-19 12:47:34 +0200

    changed terraform bcs i use aws OIDC now

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "aws_profile" {
-  description = "AWS CLI profile for Terraform"
-  type        = string
-}
-
 variable "region" {
   description = "AWS region for resources"
   type        = string
